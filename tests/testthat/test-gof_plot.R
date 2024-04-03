e2629f93b356885f9546a133701bbc3be613b49f
RichardHooijmaijers <richardhooijmaijers@gmail.com>
2024-04-03 13:24:03 +0200

    adapted check gof_plot

--- tests/testthat/test-gof_plot.R
+++ tests/testthat/test-gof_plot.R
@@ -1,4 +1,4 @@
-test_that("gof_plot works", {
+test_that("gof_plot works as expected", {
   
   res_path <- system.file("/Other/run1.res.rds", package = "shinyMixR")
   
@@ -6,23 +6,34 @@ test_that("gof_plot works", {
   
   # Note: many warnings that pollute the output
   # Note 2: the plot objects are slightly different across OS
+  #         double check; is this the case or only within shinytest2 (more related to mdoel run!)
   plot <- suppressWarnings(gof_plot(res, ptype = "all", type = "xpose"))
   expect_true(is.ggplot(plot))
   expect_equal(length(plot$layers), 8)
   
-  plot <- suppressWarnings(gof_plot(res, ptype = "all", type = "user"))
+  plot    <- suppressWarnings(gof_plot(res, ptype = "all", type = "user"))
   expect_true(is.ggplot(plot))
   expect_equal(length(plot$layers), 8)
   
+  plotlin  <- suppressWarnings(gof_plot(res, ptype = "ipred.dv", type = "user",linscale=TRUE))
+  plotlog  <- suppressWarnings(gof_plot(res, ptype = "ipred.dv", type = "user"))
+  expect_true(length(plotlin$scales$scales)==0)
+  expect_true(plotlog$scales$scales[[1]]$trans$name=="log-10")
+  expect_true(plotlog$scales$scales[[2]]$trans$name=="log-10")
+  
   for (plot_type in c("ipred.dv", "pred.dv", "idv.res", "pred.res")) {
     
-    plot <- suppressWarnings(gof_plot(res, ptype = plot_type, type = "xpose"))
+    plot <- suppressWarnings(gof_plot(res, ptype = plot_type, type = "user"))
     expect_true(is.data.frame(plot$data))
     expect_equal(nrow(plot$data), nrow(res))
     if (grepl("res", plot_type)) {
-      expect_equal(length(plot$layers), 4)
+      expect_equal(length(plot$layers), 2)
     } else {
-      expect_equal(length(plot$layers), 5)
+      expect_equal(length(plot$layers), 3)
+    }
+    if(plot_type%in%c("ipred.dv", "pred.dv")){
+      expect_equal(tolower(rlang::as_name(plot$mapping$x)), sub(".*\\.","",plot_type))  
+      expect_equal(tolower(rlang::as_name(plot$mapping$y)), sub("\\..*","",plot_type))  
     }
   }
 
