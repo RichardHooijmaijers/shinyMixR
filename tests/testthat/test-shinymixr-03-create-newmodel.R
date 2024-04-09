36cdff42e6828c432d6fef47be236658b57ed023
Veerle van Leemput <info@hypebright.nl>
2024-04-09 08:40:20 +0200

    test: use tempdir() only once

--- tests/testthat/test-shinymixr-03-create-newmodel.R
+++ tests/testthat/test-shinymixr-03-create-newmodel.R
@@ -5,13 +5,16 @@ test_that("Shiny app correctly creates new model code", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
   
+  temp_dir <- tempdir()
+  
   # Set up necessary files (internal function) - include results so we do not need to run models
-  shinyMixR:::setup_shinymixr_test(dir = paste0(tempdir(),"/files"), 
+  shinyMixR:::setup_shinymixr_test(dir = paste0(temp_dir, "/files"), 
                                    overwrite = TRUE, 
-                                   record = FALSE, incres = TRUE)
+                                   record = FALSE, 
+                                   incres = TRUE)
   
   # Start driver for Shiny test
-  app <- AppDriver$new(app_dir = paste0(tempdir(),"/files/shinyMixR/app/"), 
+  app <- AppDriver$new(app_dir = paste0(temp_dir, "/files/shinyMixR/app/"), 
                        name = "run3-model", 
                        seed = 123)
   
@@ -29,11 +32,11 @@ test_that("Shiny app correctly creates new model code", {
   app$expect_values(input = "editor-newgo")
   
   # Check if new model is created and contains correct naming
-  modmade <- "run2.r"%in%list.files(paste0(tempdir(),"/files/models"))
+  modmade <- "run2.r" %in% list.files(paste0(temp_dir, "/files/models"))
   expect_true(modmade)
   if(modmade){
-    modcont <- readLines(paste0(tempdir(),"/files/models/run2.r"))
-    expect_true(grepl("run2 <- function",modcont[1]))
+    modcont <- readLines(paste0(temp_dir, "/files/models/run2.r"))
+    expect_true(grepl("run2 <- function", modcont[1]))
   }
   
   # Perform test to save model with new name (be aware that save-as btn is a module as well)
@@ -46,16 +49,16 @@ test_that("Shiny app correctly creates new model code", {
   # Finally test if update inits works as expected (e.g. are initial changed, values itself tested outside shinytest)
   app$click("editor-updinit")
   Sys.sleep(0.5)
-  app$click("editor-goupdate",timeout_=12000)
+  app$click("editor-goupdate", timeout_ = 12000)
   app$click(selector = ".swal2-confirm")
   
-  omod  <- readLines(paste0(tempdir(),"/files/models/run1.r"))
-  amod  <- readLines(paste0(tempdir(),"/files/models/run3.r"))
-  ores  <- eval(parse(text=c("nlmixr2::nlmixr(",omod,")$ini")))
-  ares  <- eval(parse(text=c("nlmixr2::nlmixr(",amod,")$ini")))
-  expect_true(any(!(ores$est==ares$est)))
+  omod  <- readLines(paste0(temp_dir, "/files/models/run1.r"))
+  amod  <- readLines(paste0(temp_dir, "/files/models/run3.r"))
+  ores  <- eval(parse(text = c("nlmixr2::nlmixr(",omod,")$ini")))
+  ares  <- eval(parse(text = c("nlmixr2::nlmixr(",amod,")$ini")))
+  expect_true(any(!(ores$est == ares$est)))
   
   # Stop and clean up
   app$stop()
-  unlink(paste0(tempdir(),"/files"), recursive = TRUE) 
+  unlink(paste0(temp_dir, "/files"), recursive = TRUE) 
 })
