c08a29069c4d74037325943f2accd2b5c853d5de
Veerle van Leemput <info@hypebright.nl>
2024-04-16 11:21:31 +0200

    source correct files

--- R/run_shinymixr.r
+++ R/run_shinymixr.r
@@ -15,18 +15,12 @@
 #'  run_shinymixr(".")
 #' }
 run_shinymixr <- function(wd = getwd(), dry_run = FALSE, ...){
+  
   if(!file.exists(paste0(wd,"/shinyMixR/app/www"))) try(dir.create(paste0(wd,"/shinyMixR/app/www"),recursive = TRUE))
-  #if(!file.exists(paste0(wd,"/shinyMixR/app/R")))   try(dir.create(paste0(wd,"/shinyMixR/app/R"),recursive = TRUE))
   if(!file.exists(paste0(wd,"/shinyMixR/temp")))    try(dir.create(paste0(wd,"/shinyMixR/temp"),recursive=TRUE))
   
-  try(file.copy(system.file("Dashboard","global.R",package="shinyMixR"),          paste0(wd,"/shinyMixR/app/global.R"),overwrite = TRUE),silent = TRUE)
-  try(file.copy(system.file("Dashboard","server.R",package="shinyMixR"),          paste0(wd,"/shinyMixR/app/server.R"),overwrite = TRUE),silent = TRUE)
-  try(file.copy(system.file("Dashboard","logoshinyMixR.png",package="shinyMixR"), paste0(wd,"/shinyMixR/app/www/logoshinyMixR.png")),silent = TRUE)
-  
-  # We need to add the working directory to the ui.r file (global.r does not work)
-  adpt <- readLines(system.file("Dashboard","ui.R",package="shinyMixR"))
-  adpt <- c(paste0("setwd(\"",normalizePath(wd,winslash = "/"),"\")"),adpt)
-  writeLines(adpt,paste0(wd,"/shinyMixR/app/ui.R"))
+  try(file.copy(system.file("Dashboard","app.R",package="shinyMixR"),          paste0(wd,"/shinyMixR/app/app.R"),overwrite = TRUE),silent = TRUE)
+  try(file.copy(system.file("Dashboard","www/logoshinyMixR.png",package="shinyMixR"), paste0(wd,"/shinyMixR/app/www/logoshinyMixR.png")),silent = TRUE)
     
   # Clean up stuff before running the app (check if feasible or not)
   try(unlink(list.files(paste0(wd,"/shinyMixR/temp"),pattern=".*prog\\.txt$",full.names = TRUE)))
