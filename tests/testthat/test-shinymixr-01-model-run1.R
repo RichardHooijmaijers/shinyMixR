321b583b410fe7f54f848e6fd75b805652f8a0af
Veerle van Leemput <info@hypebright.nl>
2024-04-30 15:49:48 +0200

    increase timeout

--- tests/testthat/test-shinymixr-01-model-run1.R
+++ tests/testthat/test-shinymixr-01-model-run1.R
@@ -25,7 +25,7 @@ test_that("Shiny app runs model and returns parameters for run1", {
   # Wait for model to finish (NULL or 0 is the initial value, so we ignore it)
   app$wait_for_value(export = "modrun-model_updated",
                      ignore = list(NULL, 0),
-                     timeout = 120000)
+                     timeout = 240000)
   
   # Test if run is done and 'correct' results have been created
   Sys.sleep(1)
