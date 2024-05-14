c92ecac10efd51e3bf931b244470cda66d7ccd16
Veerle van Leemput <info@hypebright.nl>
2024-05-14 13:47:46 +0200

    add `nlmixr2` in imports

--- R/shinyMixR-package.R
+++ R/shinyMixR-package.R
@@ -2,6 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import nlmixr2
 #' @importFrom stats na.omit setNames
 #' @importFrom utils getParseData installed.packages read.csv tail
 #' @importFrom magrittr "%>%"
