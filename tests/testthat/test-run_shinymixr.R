7a4f9a8946c9132ceb120a5fd046d4e3525ef37b
RichardHooijmaijers <richardhooijmaijers@gmail.com>
2024-11-11 14:04:59 +0100

    added skip_on_cran to overcome deritus

--- tests/testthat/test-run_shinymixr.R
+++ tests/testthat/test-run_shinymixr.R
@@ -1,4 +1,9 @@
 test_that("run_shinymixr returns app object", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
+  
+  # Don't run this test on CI
+  skip_on_ci()
   
   app <- suppressWarnings(run_shinymixr())
   
